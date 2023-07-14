fd6ef5d46ecd91900dd6f4138f291e3f5cc664a4
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-07-14 18:34:53 +0330

    connecting store to app

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,61 +1,68 @@
-import React from 'react'
+import React from "react";
 
-import CounterControl from '../../components/CounterControl/CounterControl'
-import CounterOutput from '../../components/CounterOutput/CounterOutput'
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
+import { connect } from "react-redux";
 
 class Counter extends React.Component {
   state = {
     counter: 0,
-  }
+  };
 
   counterChangedHandler = (action, value) => {
     switch (action) {
-      case 'inc':
+      case "inc":
         this.setState((prevState) => {
-          return { counter: prevState.counter + 1 }
-        })
-        break
-      case 'dec':
+          return { counter: prevState.counter + 1 };
+        });
+        break;
+      case "dec":
         this.setState((prevState) => {
-          return { counter: prevState.counter - 1 }
-        })
-        break
-      case 'add':
+          return { counter: prevState.counter - 1 };
+        });
+        break;
+      case "add":
         this.setState((prevState) => {
-          return { counter: prevState.counter + value }
-        })
-        break
-      case 'sub':
+          return { counter: prevState.counter + value };
+        });
+        break;
+      case "sub":
         this.setState((prevState) => {
-          return { counter: prevState.counter - value }
-        })
-        break
+          return { counter: prevState.counter - value };
+        });
+        break;
     }
-  }
+  };
 
   render() {
     return (
       <div>
-        <CounterOutput value={this.state.counter} />
+        <CounterOutput value={this.props.ctr} />
         <CounterControl
           label="افزایش"
-          clicked={() => this.counterChangedHandler('inc')}
+          clicked={() => this.counterChangedHandler("inc")}
         />
         <CounterControl
           label="کاهش"
-          clicked={() => this.counterChangedHandler('dec')}
+          clicked={() => this.counterChangedHandler("dec")}
         />
         <CounterControl
           label="افزودن ۲"
-          clicked={() => this.counterChangedHandler('add', 2)}
+          clicked={() => this.counterChangedHandler("add", 2)}
         />
         <CounterControl
           label="کاهش ۲"
-          clicked={() => this.counterChangedHandler('sub', 2)}
+          clicked={() => this.counterChangedHandler("sub", 2)}
         />
       </div>
-    )
+    );
   }
 }
 
-export default Counter
+const mapStateToProps = (state) => {
+  return {
+    ctr: state.counter,
+  };
+};
+
+export default connect(mapStateToProps)(Counter);
